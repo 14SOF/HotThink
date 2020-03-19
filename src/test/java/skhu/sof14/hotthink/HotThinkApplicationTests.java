a92e0a3cf520bf43ba517887a1584a1cb0130ea6
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2020-03-19 18:53:10 +0900

    initial commit

---
+++ src/test/java/skhu/sof14/hotthink/HotThinkApplicationTests.java
@@ -0,0 +1,13 @@
+package skhu.sof14.hotthink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotThinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
