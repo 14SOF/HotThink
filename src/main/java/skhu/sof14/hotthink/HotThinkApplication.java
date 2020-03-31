7a4afd8fa39ba872cbf00e6e00840b2b2df8e411
oh950528 <42761207+oh950528@users.noreply.github.com>
2020-03-31 17:16:32 +0900

    Update HotThinkApplication.java

--- src/main/java/skhu/sof14/hotthink/HotThinkApplication.java
+++ src/main/java/skhu/sof14/hotthink/HotThinkApplication.java
@@ -2,7 +2,6 @@ package skhu.sof14.hotthink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class HotThinkApplication {
