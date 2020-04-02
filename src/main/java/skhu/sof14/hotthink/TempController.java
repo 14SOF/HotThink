c75a13e515407eff8158b96b2241b5be7d8388a4
YoungGon KIM <33260476+zerogone@users.noreply.github.com>
2020-04-03 03:11:45 +0900

    siup, 404, initial mypage, etc..

--- src/main/java/skhu/sof14/hotthink/TempController.java
+++ src/main/java/skhu/sof14/hotthink/TempController.java
@@ -1,7 +1,9 @@
 package skhu.sof14.hotthink;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TempController {
@@ -9,5 +11,20 @@ public class TempController {
     @GetMapping("login")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("signup")
+    public String signUp(){
+        return "signup";
+    }
+
+    @GetMapping("404")
+    public String pageNotFound(){
+        return "404";
+    }
+    @GetMapping("mypage")
+    public String myPage(Model model){
+        model.addAttribute("test", "김영곤");
+        return "mypage";
     }
 }
