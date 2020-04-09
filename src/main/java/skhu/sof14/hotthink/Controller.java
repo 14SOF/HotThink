78064f3dd1c98059fd99f7022af09b96a740a6b7
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-04-09 16:59:36 +0900

    컨트롤러 이름 수정

--- src/main/java/skhu/sof14/hotthink/Controller.java
+++ src/main/java/skhu/sof14/hotthink/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class tempcontroller {
+public class Controller {
 
     @GetMapping("qna")
     public String qna(){
