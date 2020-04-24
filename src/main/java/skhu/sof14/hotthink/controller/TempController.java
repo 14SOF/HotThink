c26e4379149ef084f1b41c95c07763551fd43057
YoungGon KIM <33260476+zerogone@users.noreply.github.com>
2020-04-24 22:08:01 +0900

    1. 404 컨트롤 2. 디비 한글 오류 해결

--- src/main/java/skhu/sof14/hotthink/controller/TempController.java
+++ src/main/java/skhu/sof14/hotthink/controller/TempController.java
@@ -1,7 +1,6 @@
 package skhu.sof14.hotthink.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
